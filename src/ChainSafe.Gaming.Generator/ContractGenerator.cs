55852d749ac642d85c15ca8b7404dde895f7a3e4
Youtpout <EddyBoughioul@hotmail.fr>
2024-05-27 21:48:29 +0200

    remove nethereum generator and use a custom generator

--- src/ChainSafe.Gaming.Generator/ContractGenerator.cs
+++ src/ChainSafe.Gaming.Generator/ContractGenerator.cs
@@ -1,22 +1,117 @@
-﻿using ChainSafe.Gaming.Generator.Templates;
-using Nethereum.Generators.Model;
-using Nethereum.Generators.Service;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using Nethereum.ABI.ABIDeserialisation;
+using System.Linq;
+using System.Text;
 
 namespace ChainSafe.Gaming.Generator
 {
     public class ContractGenerator
     {
+        public static object SpaceUtils { get; private set; }
+
         public static string Generate(string abi, string contractName)
         {
-            var contractAbi = new Nethereum.Generators.Net.GeneratorModelABIDeserialiser().DeserialiseABI(abi);
-            var classModel = new ServiceModel(contractAbi, contractName, null, contractName, contractName, contractName);
-            var template = new ServiceChainsafeTemplate(classModel);
-            return template.GenerateClass();
+            var contractAbi = ABIDeserialiserFactory.DeserialiseContractABI(abi);
+            return GenerateClass(contractAbi, contractName);
+        }
+
+        private static string GenerateClass(Nethereum.ABI.Model.ContractABI contractAbi, string contractName)
+        {
+            return
+                           $@"using ChainSafe.Gaming.Evm.Providers;
+using ChainSafe.Gaming.Evm.Signers;
+using ChainSafe.Gaming.Web3.Analytics;
+using ChainSafe.Gaming.Web3.Core.Evm;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace ChainSafe.Gaming
+{{
+    public partial class {contractName}: GeneratedContract
+    {{
+        public {contractName}(string abi, string address, IRpcProvider provider, ISigner signer = null, ITransactionExecutor transactionExecutor = null, IAnalyticsClient analyticsClient = null) : base(abi, address, provider, signer, transactionExecutor, analyticsClient)
+        {{
+        }}
+
+{GenerateMethods(contractAbi.Functions)}
+    }}
+}}";
+        }
+
+        private static string GenerateMethods(Nethereum.ABI.Model.FunctionABI[] functionsList)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (functionsList?.Length > 0)
+            {
+
+                foreach (var function in functionsList)
+                {
+                    sb.AppendLine(GenerateFunction(function));
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
         }
 
 
+        private static string GenerateFunction(Nethereum.ABI.Model.FunctionABI functionAbi)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"        public ");
+            if (functionAbi.OutputParameters?.Length > 0)
+            {
+                // if we have output parameter we return the first
+                foreach (var outputParameter in functionAbi.OutputParameters)
+                {
+                    // todo support multiple output
+                    sb.Append($"{outputParameter.Type.ToString()}");
+                    break;
+                }
+            }
+            else
+            {
+                // if we have don't output parameter we return object array
+                sb.Append("object[]");
+            }
+            sb.Append($" {functionAbi.Name}(");
+            if (functionAbi.InputParameters.Length > 0)
+            {
+                // support type
+                var inputs = functionAbi.InputParameters.Select(x => $"string {x.Name}");
+                sb.Append($"{string.Join(",", inputs)}");
+            }
+            sb.Append($")");
+            sb.AppendLine($"        {{");
+
+            if (functionAbi.Constant)
+            {
+                sb.Append($"object[] response = this.Contract.Call(\"{functionAbi.Name}\"");
+
+            }
+            else
+            {
+                sb.Append($"object[] response = this.Contract.Send(\"{functionAbi.Name}\"");
+            }
+
+            if (functionAbi.InputParameters.Length > 0)
+            {
+                // generate the Object[] parameter
+                var inputs = functionAbi.InputParameters.Select(x => x.Name);
+                sb.Append($", new Object[] {{{string.Join(",", inputs)}}}");
+            }
+            sb.Append(");");
+            sb.AppendLine();
+            if (functionAbi.OutputParameters?.Length > 0)
+            {
+                // todo support multiple output
+                sb.Append($"return response[0] as {functionAbi.OutputParameters[0].Type.ToString()};");
+            }
+            else
+            {
+                sb.Append($"return response;");
+            }
+            sb.Append($"        }}");
+            return sb.ToString();
+        }
     }
+
 }
--- src/ChainSafe.Gaming.Generator/Test.cs
+++ src/ChainSafe.Gaming.Generator/Test.cs
@@ -1,39 +1,107 @@
 ﻿using ChainSafe.Gaming.Evm.Providers;
 using ChainSafe.Gaming.Evm.Signers;
+using ChainSafe.Gaming.Generator;
 using ChainSafe.Gaming.Web3.Analytics;
 using ChainSafe.Gaming.Web3.Core.Evm;
+using NBitcoin;
 using System;
-using System.Collections.Generic;
 using System.Numerics;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace ChainSafe.Gaming.Generator
+namespace ChainSafe.Gaming
 {
-    public class NftTest : GeneratedContract
+    public partial class NFTContract : GeneratedContract
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NftTest"/> class.
-        /// </summary>
-        /// <param name="abi">The abi.</param>
-        /// <param name="address">The contract address.</param>
-        /// <param name="provider">The RPC provider.</param>
-        /// <param name="signer">The signer.</param>
-        /// <param name="transactionExecutor">Transaction executor.</param>
-        public NftTest(string abi, string address, IRpcProvider provider, ISigner signer = null, ITransactionExecutor transactionExecutor = null, IAnalyticsClient analyticsClient = null)
-            : base(abi, address, provider, signer, transactionExecutor, analyticsClient)
+        public NFTContract(string abi, string address, IRpcProvider provider, ISigner signer = null, ITransactionExecutor transactionExecutor = null, IAnalyticsClient analyticsClient = null) : base(abi, address, provider, signer, transactionExecutor, analyticsClient)
         {
         }
 
-        /// <summary>
-        /// Mint a new nft.
-        /// </summary>
-        /// <param name="id">id of the nft</param>
-        /// <returns>id of the new nft</returns>
-        public async Task<BigInteger> Mint(BigInteger id)
+        public object[] approve(string to, string tokenId)
         {
-            object[] response = await Contract.Send("mint", new object[] { id });
-            return BigInteger.Parse(response[0].ToString());
+            object[] response = this.Contract.Send(approve, new Object[] { to, tokenId });
+            return response;
         }
+        public object[] renounceOwnership()
+        {
+            object[] response = this.Contract.Send(renounceOwnership);
+            return response;
+        }
+        public object[] safeMint(string to)
+        {
+            object[] response = this.Contract.Send(safeMint, new Object[] { to });
+            return response;
+        }
+        public object[] safeTransferFrom(string from, string to, string tokenId)
+        {
+            object[] response = this.Contract.Send(safeTransferFrom, new Object[] { from, to, tokenId });
+            return response;
+        }
+        public object[] safeTransferFrom(string from, string to, string tokenId, string data)
+        {
+            object[] response = this.Contract.Send(safeTransferFrom, new Object[] { from, to, tokenId, data });
+            return response;
+        }
+        public object[] setApprovalForAll(string operator, string approved)
+        {
+            object[] response = this.Contract.Send(setApprovalForAll, new Object[] {operator, approved });
+            return response;
+        }
+        public object[] transferFrom(string from, string to, string tokenId)
+        {
+            object[] response = this.Contract.Send(transferFrom, new Object[] { from, to, tokenId });
+            return response;
+        }
+        public object[] transferOwnership(string newOwner)
+        {
+            object[] response = this.Contract.Send(transferOwnership, new Object[] { newOwner });
+            return response;
+        }
+        public uint256 balanceOf(string owner)
+        {
+            object[] response = this.Contract.Call(balanceOf, new Object[] { owner });
+            return response[0] as uint256;
+        }
+        public address getApproved(string tokenId)
+        {
+            object[] response = this.Contract.Call(getApproved, new Object[] { tokenId });
+            return response[0] as address;
+        }
+        public bool isApprovedForAll(string owner, string operator)
+        {
+            object[] response = this.Contract.Call(isApprovedForAll, new Object[] { owner,operator});
+            return response[0] as bool;
+        }
+        public string name()
+        {
+            object[] response = this.Contract.Call(name);
+            return response[0] as string;
+        }
+        public address owner()
+        {
+            object[] response = this.Contract.Call(owner);
+            return response[0] as address;
+        }
+        public address ownerOf(string tokenId)
+        {
+            object[] response = this.Contract.Call(ownerOf, new Object[] { tokenId });
+            return response[0] as address;
+        }
+        public bool supportsInterface(string interfaceId)
+        {
+            object[] response = this.Contract.Call(supportsInterface, new Object[] { interfaceId });
+            return response[0] as bool;
+        }
+        public string symbol()
+        {
+            object[] response = this.Contract.Call(symbol);
+            return response[0] as string;
+        }
+        public string tokenURI(string tokenId)
+        {
+            object[] response = this.Contract.Call(tokenURI, new Object[] { tokenId });
+            return response[0] as string;
+        }
+
+
     }
 }
