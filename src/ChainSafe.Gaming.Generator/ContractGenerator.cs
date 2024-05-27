c24539221b42cb4f40387be7b1b14f3014948e23
Youtpout <EddyBoughioul@hotmail.fr>
2024-05-27 13:14:54 +0200

    add project to generate custom contract class

---
+++ src/ChainSafe.Gaming.Generator/ContractGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ChainSafe.Gaming.Generator
+{
+    public class ContractGenerator
+    {
+        public static void Generate(string abi)
+        {
+            var contractAbi = new Nethereum.Generators.Net.GeneratorModelABIDeserialiser().DeserialiseABI(abi);
+        }
+
+
+    }
+}
